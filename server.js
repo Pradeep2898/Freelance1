b4fc140646e06753acf63f20a795e6441621d195
Pradeep Anand <spanand2898@gmail.com>
2021-04-23 23:02:30 +0530

    server.js file issue

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the Mk1 directory
-app.use(express.static(__dirname + '/Mk1'));
+app.use(express.static(__dirname + '/angularapp'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/Mk1/index.html'));
+res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
