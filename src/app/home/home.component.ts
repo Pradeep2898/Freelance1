67955a6798f374bbdeaa0960c1563cd1a90f8a47
Pradeep Anand <spanand2898@gmail.com>
2021-05-03 01:31:54 +0530

    editing how it works section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,16 +11,13 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     $(document).ready(()=> {
-      $(".vertical-tabs-steps .nav-link, .horizontal-tabs-steps .nav-link").click(()=> {
-        $(this).parent().prevAll().children('.vertical-tabs-steps .nav-link, .horizontal-tabs-steps .nav-link').addClass('checked-steps');
-
-        $(this).parent().nextAll().children('.vertical-tabs-steps .nav-link, .horizontal-tabs-steps .nav-link').removeClass('checked-steps');
-
+      $(".horizontal-tabs-steps .nav-link").click(()=> {
+        $(this).parent().prevAll().children('.horizontal-tabs-steps .nav-link').addClass('checked-steps');
+        $(this).parent().nextAll().children('.horizontal-tabs-steps .nav-link').removeClass('checked-steps');
         $(this).removeClass('checked-steps');
         $(this).parent().removeClass('complete-step');
         $(this).parent().nextAll().removeClass('complete-step');
-
-        $(".horizontal-tabs-steps .nav-link.checked-steps, .vertical-tabs-steps .nav-link.checked-steps").parent().addClass('complete-step');
+        $(".horizontal-tabs-steps .nav-link.checked-steps").parent().addClass('complete-step');
       });
     });
   }
